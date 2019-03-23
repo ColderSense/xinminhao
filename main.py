7313f863b52b59eb4a9ac342fa80cc1096f7b3c5
ColderSense <lee_cq@qq.com>
2019-03-23 13:04:27 +0000

    second edition

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 #！/usr/bin/python
 
 from flask import Flask, render_template, Response, jsonify, request
-
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
 # start flask app
 app = Flask(__name__)
 
@@ -11,6 +13,34 @@ app = Flask(__name__)
 def index():
 	"""Video streaming home page."""
 	return render_template('index.html')
+@app.route('/feedback')
+def feedback():
+	"""Video streaming home page."""
+	return render_template('feedback.html')
+	
+@app.route('/send_email', methods=['POST'])
+def send_email():
+	sender = 'leecqchn@163.com'
+	receiver = 'lee_cq@qq.com'
+	data = request.get_json()
+	name =data['name']
+	email =data['email']
+	message = data['message']
+	print(name, email, message)
+	
+	send_msg = 'The person ' + name + ' has send some message to you,' + 'the email is: ' + email + ', the feedback message is: ' + message
+	msg=MIMEText(send_msg,'plain','utf-8')
+	msg['From']=formataddr([name,sender])   #括号里的对应发件人邮箱昵称、发件人邮箱账号
+	msg['To']=formataddr(["lichaoqun",receiver])   #括号里的对应收件人邮箱昵称、收件人邮箱账号
+	msg['Subject']="xinminhao_feedback" #邮件的主题，也可以说是标题
+		
+	smtp = smtplib.SMTP() 
+	smtp.connect('smtp.163.com',25) 
+	smtp.login('leecqchn@163.com', 'lcq19930819') 
+	smtp.sendmail(sender, receiver, msg.as_string()) 
+	smtp.quit()
+	print(jsonify({"msg": "success"}))
+	return "success"
 
 
 if __name__ == "__main__":
